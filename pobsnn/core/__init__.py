ab089853bfec83309b8b3604c6b4a393c94883cc
ROBERT CECIL <146335780+lastforkbender@users.noreply.github.com>
2026-07-01 21:44:00 -0500

    Initialize core module with spline and neuron classes

---
+++ pobsnn/core/__init__.py
@@ -0,0 +1,10 @@
+from .spline_basis import BSplineBasis, BSplineBasisConfig
+from .bspline_neuron import BSplineNeuron
+from .bspline_layer import BSplineLayer
+
+__all__ = [
+    "BSplineBasis",
+    "BSplineBasisConfig",
+    "BSplineNeuron",
+    "BSplineLayer",
+]
