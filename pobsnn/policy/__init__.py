f2d11c2cc0b9b79bf5a0e2350649814ebcc61e41
ROBERT CECIL <146335780+lastforkbender@users.noreply.github.com>
2026-07-11 23:42:30 -0500

    Delete pobsnn/policy directory

--- pobsnn/policy/__init__.py
+++
@@ -1,8 +0,0 @@
-from .proposal import PolicyProposal, ProposalSeverity
-from .engine import PolicyDecision, PolicyEngine
-
-__all__ = ["PolicyProposal", "ProposalSeverity", "PolicyDecision", "PolicyEngine"]
-
-from .controller_update_policy import ControllerUpdatePolicy, ControllerUpdateDecision
-
-__all__ = [name for name in globals() if not name.startswith("_")]
